347aaaa801db062d4879befdad1ed9590f0bfb67
lucasil <lucas07.macedo@gmail.com>
2024-11-21 17:45:47 -0300

    menu creation

--- src/main/java/com/lucassilveira/receitario/config/WebSecurityConfig.java
+++ src/main/java/com/lucassilveira/receitario/config/WebSecurityConfig.java
@@ -16,7 +16,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/login", "/recover", "/reset-password", "/reset-password-success", "/css/**", "/js/**", "/images/**").permitAll() // Permitir acesso sem autenticação
+                .requestMatchers("/login", "/recover", "/reset-password", "/reset-password-success", "/css/**", "/js/**", "/images/**", "/templates/**").permitAll() // Permitir acesso sem autenticação
                 .requestMatchers("/admin/**"). hasRole("Administrador")
                 .requestMatchers("/chef/**"). hasRole("Cozinheiro")
                 .requestMatchers("/taster/**"). hasRole("Degustador")
