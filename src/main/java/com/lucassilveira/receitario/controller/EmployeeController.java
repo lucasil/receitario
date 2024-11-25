7b4d70d19195378ff4e121c31790dfde67e2e9a3
lucasil <lucas07.macedo@gmail.com>
2024-11-24 23:48:43 -0300

    register correction

--- src/main/java/com/lucassilveira/receitario/controller/EmployeeController.java
+++ src/main/java/com/lucassilveira/receitario/controller/EmployeeController.java
@@ -6,14 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.lucassilveira.receitario.model.Employee;
 import com.lucassilveira.receitario.model.Role;
 import com.lucassilveira.receitario.repository.EmployeeRepository;
 import com.lucassilveira.receitario.repository.RoleRepository;
 import com.lucassilveira.receitario.service.RoleService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class EmployeeController {
@@ -42,28 +47,63 @@ public class EmployeeController {
     @Autowired
     private RoleService roleService;
 
-    // Exibe o formulário de cadastro de usuário
-    @GetMapping("/admin/register")
-    public String showRegistrationForm(Model model) {
+    // Tela de apresentação de usuários
+
+    // Método para exibir a lista de usuários na página
+    @GetMapping("/admin/users")
+    public String showUsersList(Model model) {
+        // Obtém todos os usuários e papeis do banco de dados
+        List<Employee> employees = employeeRepository.findAll();
         List<Role> roles = roleService.getAllRoles();
+        
+        // Adiciona a lista de usuários e papeis ao modelo
+        model.addAttribute("employees", employees);
         model.addAttribute("roles", roles);
-        model.addAttribute("employee", new Employee());
-        return "admin/register";
+
+        // Retorna o nome da página HTML (view) que vai exibir a lista de usuários
+        return "admin/users";
     }
 
-    @PostMapping("/admin/register")
-    public String registerUser(Employee employee) {
+    @PostMapping("/admin/users")
+    public String registerUser(@ModelAttribute @Valid Employee employee,
+                                BindingResult bindingResult,
+                                @RequestParam("role") int roleId) {
+        // Se houver erros de validação, retorne para o formulário com os erros
+        if (bindingResult.hasErrors()) {
+            return "admin/users";
+        }
+        
         // Codifica a senha antes de salvar no banco
         employee.setPassword(passwordEncoder.encode(employee.getPassword()));
 
+        employee.setActive(true);
+
         // Define um papel padrão (exemplo: papel "USER")
-        Role selectedRole = roleRepository.findByName("Administrador");
-        employee.setRole(selectedRole);
+        Role selectedRole = roleRepository.findById(roleId);
+
+        if (selectedRole != null) {
+            employee.setRole(selectedRole);
+        } else {
+            return "redirect:/admin/users?error";
+        }
 
         // Salva o novo usuário no banco de dados
         employeeRepository.save(employee);
 
-        return "redirect:/admin/register?success"; // Redireciona com mensagem de sucesso
+        return "redirect:/admin/users?success"; // Redireciona com mensagem de sucesso
     }
 
+    @PostMapping("/admin/deactivate")
+    public String deactivateUser(@RequestParam("id") int id, 
+                                    Model model) {
+        Employee employee = employeeRepository.findById(id).orElse(null);
+        if (employee != null) {
+            employee.setActive(false); // Desativa o usuário
+            employeeRepository.save(employee); // Salva a alteração
+        }
+
+        model.addAttribute("employees", employeeRepository.findAll());
+        
+        return "admin/users";
+    } 
 }
